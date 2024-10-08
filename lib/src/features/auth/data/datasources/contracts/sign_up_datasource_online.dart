95075e0c04c2c57aa01972c04ca3407b921327f5
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-10-08 19:11:23 +0300

    implement sign up datasource

--- lib/src/features/auth/data/datasources/contracts/sign_up_datasource_online.dart
+++ lib/src/features/auth/data/datasources/contracts/sign_up_datasource_online.dart
@@ -1,7 +1,8 @@
 import 'package:online_exam_app/src/features/auth/data/api/models/request/sign_up_model.dart';
 import 'package:online_exam_app/src/features/auth/data/api/models/response/sign_up_response.dart';
+import 'package:online_exam_app/src/features/auth/domain/core/result.dart';
 
 abstract class SignUpDatasourceOnline {
 
-  Future<SignUpResponse> signUp({required SignUpModel signUpModel});
+  Future<Result<SignUpResponse>> signUp({required SignUpModel signUpModel});
 }
