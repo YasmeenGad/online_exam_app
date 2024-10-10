edf08b1d7c2775d2f7e96f1cf2ce691f0ab589ac
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-10-10 10:56:21 +0300

    create validators in validator class

--- lib/src/features/auth/presentation/views/signup_view.dart
+++ lib/src/features/auth/presentation/views/signup_view.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:online_exam_app/src/core/styles/app_colors.dart';
+import 'package:online_exam_app/src/core/styles/app_styles.dart';
 import 'package:online_exam_app/src/features/auth/presentation/widgets/custom_signup_appbar.dart';
-import 'package:online_exam_app/src/features/auth/presentation/widgets/custom_signup_button_section.dart';
-import 'package:online_exam_app/src/features/auth/presentation/widgets/text_field_section.dart';
+import 'package:online_exam_app/src/features/auth/presentation/widgets/custom_text_form_field.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController usernameController = TextEditingController();
+    TextEditingController firstNameController = TextEditingController();
+    TextEditingController lastNameController = TextEditingController();
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+    TextEditingController confirmPasswordController = TextEditingController();
+    TextEditingController phoneNumberController = TextEditingController();
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(top: 56, left: 16, right: 16),
@@ -22,7 +31,77 @@ class SignUpView extends StatelessWidget {
               ),
             ),
             SliverToBoxAdapter(
-                child: TextFieldSection()),
+                child: Form(
+              child: Column(
+                children: [
+                  CustomTextFormField(
+                    controller: usernameController,
+                    hintText: 'Enter your user name',
+                    labelText: 'User Name',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: CustomTextFormField(
+                        controller: firstNameController,
+                        hintText: 'Enter first name',
+                        labelText: 'First Name',
+                      )),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                          child: CustomTextFormField(
+                        controller: lastNameController,
+                        hintText: 'Enter last name',
+                        labelText: 'Last Name',
+                      )),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CustomTextFormField(
+                    controller: emailController,
+                    hintText: 'Enter your Email',
+                    labelText: 'Email',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: CustomTextFormField(
+                        controller: passwordController,
+                        hintText: 'Enter password',
+                        labelText: 'Passwoed',
+                      )),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                          child: CustomTextFormField(
+                        controller: confirmPasswordController,
+                        hintText: 'Confirm password',
+                        labelText: 'Password',
+                      )),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CustomTextFormField(
+                    controller: phoneNumberController,
+                    hintText: 'Enter phone number',
+                    labelText: 'Phone Number',
+                  ),
+                ],
+              ),
+            )),
             SliverToBoxAdapter(
               child: SizedBox(
                 height: 32,
@@ -30,7 +109,43 @@ class SignUpView extends StatelessWidget {
             ),
             SliverToBoxAdapter(
                 child: Container(
-              child: CustomSignupButtonSection()
+              child: Column(
+                children: [
+                  Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      height: 48,
+                      decoration: BoxDecoration(
+                        color: AppColors.blueBaseColor,
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Sign Up",
+                          style: AppStyles.styleMedium16(context)
+                              .copyWith(color: AppColors.whiteColor),
+                        ),
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have an account?",
+                        style: AppStyles.styleRegular14(context)
+                            .copyWith(color: AppColors.blackBaseColor),
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Login",
+                              style: AppStyles.styleMedium16(context).copyWith(
+                                color: AppColors.blueBaseColor,
+                                decoration: TextDecoration.underline,
+                              )))
+                    ],
+                  )
+                ],
+              ),
             )),
           ],
         ),
--- lib/src/features/auth/presentation/widgets/custom_text_form_field.dart
+++ lib/src/features/auth/presentation/widgets/custom_text_form_field.dart
@@ -2,22 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:online_exam_app/src/core/styles/app_colors.dart';
 import 'package:online_exam_app/src/core/styles/app_styles.dart';
 
-class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key, this.hintText, this.labelText});
+class CustomTextFormField extends StatefulWidget {
+  const CustomTextFormField({super.key, this.hintText, this.labelText, this.controller});
   final String? hintText;
   final String? labelText;
+  final dynamic controller;
 
   @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  @override
   Widget build(BuildContext context) {
-    return Form(child: TextFormField(
-                    decoration: InputDecoration(
-                      floatingLabelBehavior: FloatingLabelBehavior.always,
-                      labelText: '${labelText ?? ''}',
-                      labelStyle: AppStyles.styleRegular12(context).copyWith(color: AppColors.grayColor),
-                      hintText: '${hintText ?? ''}',
-                      hintStyle: AppStyles.styleRegular14(context).copyWith(color: AppColors.placeHolderColor),
-                      border: OutlineInputBorder(),
-                    ),
-                  ));
+    return TextFormField(
+          controller: widget.controller,
+          decoration: InputDecoration(
+    floatingLabelBehavior: FloatingLabelBehavior.always,
+    labelText: '${widget.labelText ?? ''}',
+    labelStyle: AppStyles.styleRegular12(context)
+        .copyWith(color: AppColors.grayColor),
+    hintText: '${widget.hintText ?? ''}',
+    hintStyle: AppStyles.styleRegular14(context)
+        .copyWith(color: AppColors.placeHolderColor),
+    border: OutlineInputBorder(),
+          ),
+        );
   }
 }
