bdcff26ae149de6fe56160dd802a1f8cb23e103a
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-10-19 12:52:29 +0300

    implement profile exception class

---
+++ lib/src/features/profile/domain/core/profile_exception.dart
@@ -0,0 +1,32 @@
+class AppException implements Exception {
+  final String? message;
+  final String? details;
+
+  AppException({required this.message, this.details});
+
+  @override
+  String toString() {
+    return message ?? '';
+  }
+}
+
+class UnauthorizedException extends AppException {
+  UnauthorizedException({String? message}) : super(message: message);
+}
+
+class NoInternetException extends AppException {
+  NoInternetException({String? message}) : super(message: message);
+}
+
+class ServerError extends AppException {
+  ServerError({String? message, String? details})
+      : super(message: message, details: details);
+}
+
+class ParsingError extends AppException {
+  ParsingError({String? message}) : super(message: message);
+}
+
+class UnknownErrorException extends AppException {
+  UnknownErrorException({String? message}) : super(message: message);
+}
