f36c9029d950ff506c258a1ce3700e2d37314fd4
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-10-21 04:03:51 +0300

    impl useCase

--- lib/src/features/profile/domain/usecases/profile_usecase.dart
+++ lib/src/features/profile/domain/usecases/profile_usecase.dart
@@ -3,6 +3,8 @@ import 'package:online_exam_app/src/features/profile/domain/core/profile_%20resu
 import 'package:online_exam_app/src/features/profile/domain/entities/response/profile_data_response.dart';
 
 import '../contracts/profile_repository.dart';
+import '../entities/request/change_password_request_entity.dart';
+import '../entities/response/change_password_response_entity.dart';
 
 @injectable
 class ProfileUseCase {
@@ -14,5 +16,10 @@ class ProfileUseCase {
   Future<ProfileResult<ProfileDataResponse>> getProfileData(
       String token) async {
     return await _profileRepository.getProfileData(token);
+  }
+
+  Future<ProfileResult<ChangePasswordResponseEntity>> changePassword(
+      String token, ChangePasswordRequestEntity request) async {
+    return await _profileRepository.changePassword(token, request);
   }
 }
