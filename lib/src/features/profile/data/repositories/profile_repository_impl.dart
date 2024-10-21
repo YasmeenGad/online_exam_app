3a3257c79c8ad48208eeb9b9df95402fafff0339
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-10-21 04:02:55 +0300

    impl repository

--- lib/src/features/profile/data/repositories/profile_repository_impl.dart
+++ lib/src/features/profile/data/repositories/profile_repository_impl.dart
@@ -1,6 +1,8 @@
 import 'package:injectable/injectable.dart';
 import 'package:online_exam_app/src/features/profile/data/datasources/contracts/online_datasource/profile_datasource.dart';
 import 'package:online_exam_app/src/features/profile/domain/core/profile_%20result.dart';
+import 'package:online_exam_app/src/features/profile/domain/entities/request/change_password_request_entity.dart';
+import 'package:online_exam_app/src/features/profile/domain/entities/response/change_password_response_entity.dart';
 import 'package:online_exam_app/src/features/profile/domain/entities/response/profile_data_response.dart';
 
 import '../../domain/contracts/profile_repository.dart';
@@ -16,6 +18,13 @@ class ProfileRepositoryImpl implements ProfileRepository {
   Future<ProfileResult<ProfileDataResponse>> getProfileData(
       String token) async {
     var response = await _profileDataSource.getProfileData(token);
+    return response;
+  }
+
+  @override
+  Future<ProfileResult<ChangePasswordResponseEntity>> changePassword(
+      String token, ChangePasswordRequestEntity request) async {
+    var response = await _profileDataSource.changePassword(token, request);
     return response;
   }
 }
