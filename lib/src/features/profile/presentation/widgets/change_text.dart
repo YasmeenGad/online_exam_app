ee2db6098b2cea49abcbf6c5ac497c966a5f757c
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-10-26 14:20:06 +0300

    refactor language view

--- lib/src/features/profile/presentation/widgets/change_text.dart
+++ lib/src/features/profile/presentation/widgets/change_text.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../../../core/routes/routes_name.dart';
 import '../../../../core/styles/app_colors.dart';
 import '../../../../core/styles/app_styles.dart';
@@ -14,7 +14,7 @@ class ChangeText extends StatelessWidget {
         Navigator.pushNamed(context, RoutesName.resetPasswordProfileView);
       },
       child: Text(
-        "Change",
+        "${AppLocalizations.of(context)!.change}",
         style: AppStyles.styleMedium12(context)
             .copyWith(color: AppColors.blueBaseColor),
       ),
