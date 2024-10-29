021434feb5bb582c6e0da4ebb70db4c614d5158b
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-10-29 18:16:16 +0200

    create questions view

--- lib/src/core/routes/app_routes.dart
+++ lib/src/core/routes/app_routes.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:online_exam_app/src/core/global/layout.dart';
 import 'package:online_exam_app/src/core/routes/routes_name.dart';
 import 'package:online_exam_app/src/features/auth/presentation/views/forget_password_view.dart';
-import 'package:online_exam_app/src/features/auth/presentation/views/welcome_view.dart';
 import 'package:online_exam_app/src/features/auth/presentation/views/login_view.dart';
 import 'package:online_exam_app/src/features/auth/presentation/views/signup_view.dart';
+import 'package:online_exam_app/src/features/auth/presentation/views/welcome_view.dart';
 import 'package:online_exam_app/src/features/exam/presentation/views/exam_details_view.dart';
 import 'package:online_exam_app/src/features/profile/presentation/views/profile_view.dart';
 import 'package:online_exam_app/src/features/profile/presentation/views/reset_password_profile_view.dart';
-
+import 'package:online_exam_app/src/features/questions/presentation/views/questions_view.dart';
 import '../../features/auth/data/datasources/contracts/offline_auth_datasource.dart';
 import '../../features/auth/presentation/views/email_verification_view.dart';
 import '../../features/auth/presentation/views/reset_password_view.dart';
-import 'package:online_exam_app/src/core/global/layout.dart';
-
 import '../../features/exam/presentation/views/exam_view.dart';
 import '../../features/exam/presentation/widgets/exam_argument.dart';
 import '../../features/splash/view/splash_view.dart';
@@ -26,32 +25,34 @@ class AppRoutes {
       RoutesName.loginView: (context) => const LoginView(),
       RoutesName.signUpView: (context) => const SignUpView(),
       RoutesName.forgetPasswordView: (context) => const ForgetPasswordView(),
-      RoutesName.emailVerificationView: (context) => const EmailVerificationView(),
+      RoutesName.emailVerificationView: (context) =>
+          const EmailVerificationView(),
       RoutesName.resetPasswordView: (context) => const ResetPasswordView(),
-      RoutesName.welcomeView: (context) =>  WelcomeView(),
-      RoutesName.bottomNavigationBar: (context) =>  Layout(),
+      RoutesName.welcomeView: (context) => WelcomeView(),
+      RoutesName.bottomNavigationBar: (context) => Layout(),
       RoutesName.profileView: (context) => ProfileView(),
       RoutesName.resetPasswordProfileView: (context) =>
           ResetPasswordProfileView(),
       RoutesName.examView: (context) {
-        final arguments = ModalRoute.of(context)!.settings.arguments as ExamArguments;
+        final arguments =
+            ModalRoute.of(context)!.settings.arguments as ExamArguments;
         return ExamView(
           subjectId: arguments.subjectId!,
           subjectImage: arguments.subjectImage!,
         );
       },
-      RoutesName.splashView: (context) =>  SplashView(
-         offlineAuthDataSource: getIt<OfflineAuthDataSource>(),
-      ),
+      RoutesName.splashView: (context) => SplashView(
+            offlineAuthDataSource: getIt<OfflineAuthDataSource>(),
+          ),
       RoutesName.examDetailView: (context) {
-    final arguments = ModalRoute.of(context)!.settings.arguments as ExamArguments;
-    return ExamDetailsView(
-      examId: arguments.subjectId!,
-      subjectImage: arguments.subjectImage!,
-    );
-  },
-
-
+        final arguments =
+            ModalRoute.of(context)!.settings.arguments as ExamArguments;
+        return ExamDetailsView(
+          examId: arguments.subjectId!,
+          subjectImage: arguments.subjectImage!,
+        );
+      },
+      RoutesName.questionsView: (context) => QuestionsView()
     };
   }
 }
