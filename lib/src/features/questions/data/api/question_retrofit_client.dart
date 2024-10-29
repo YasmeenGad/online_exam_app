3838dd64a8538e2e226de2a758f3ec9220e73e2f
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-10-29 02:56:36 +0200

    add get question function header in retrofit client

--- lib/src/features/questions/data/api/question_retrofit_client.dart
+++ lib/src/features/questions/data/api/question_retrofit_client.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
+import 'package:online_exam_app/src/features/questions/data/api/models/response/question_response_dto.dart';
 import 'package:retrofit/retrofit.dart';
+
 import '../../../../core/constants/app_apis.dart';
 
 part 'question_retrofit_client.g.dart';
@@ -11,4 +13,10 @@ part 'question_retrofit_client.g.dart';
 abstract class QuestionRetrofitClient {
   @factoryMethod
   factory QuestionRetrofitClient(Dio dio) = _QuestionRetrofitClient;
+
+  @GET(AppApis.getQuestions)
+  Future<QuestionResponseDto> getQuestions(
+    @Header("token") String token,
+    @Query("exam") String examId,
+  );
 }
