0233d53fd7afb8e477088bedf979230bb13e34ac
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-11-05 02:23:11 +0200

    impl cubit

--- lib/src/features/questions/presentation/cubit/questions_action.dart
+++ lib/src/features/questions/presentation/cubit/questions_action.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
+import '../../domain/entities/request/check_question_request_entity.dart';
+
 sealed class QuestionsAction {}
 
 class GetQuestionsAction extends QuestionsAction {
@@ -7,4 +9,11 @@ class GetQuestionsAction extends QuestionsAction {
   BuildContext context;
 
   GetQuestionsAction({required this.context, required this.examId});
+}
+
+class CheckQuestionAction extends QuestionsAction {
+  CheckQuestionRequestEntity request;
+  BuildContext context;
+
+  CheckQuestionAction({required this.context, required this.request});
 }
