04d5b3ad17377b48c29d6758bc639ceffef7b15d
YasmeenGad <gadyasmeen335@gmail.com>
2025-01-20 04:58:11 +0200

    chore: merge question feature

---
+++ lib/src/features/questions/data/datasource/impl/questions_offline_datasource_impl.dart
@@ -0,0 +1,52 @@
+import 'package:hive/hive.dart';
+import 'package:injectable/injectable.dart';
+import 'package:online_exam_app/src/features/questions/domain/entities/response/check_question_response_entity.dart';
+import 'package:online_exam_app/src/features/questions/domain/entities/response/question_response_entity.dart';
+
+import '../../../domain/entities/request/check_question_request_entity.dart';
+import '../contracts/offline_datasource/question_offline_datasource.dart';
+
+@Injectable(as: QuestionsOfflineDatasource)
+class QuestionsOfflineDatasourceImpl implements QuestionsOfflineDatasource {
+  static const String _questionBox = 'questionBox';
+  static const String _checkQuestionBox = 'checkQuestionBox';
+  static const String _questionKey = 'questions';
+  static const String _checkQuestionKey = 'checkQuestions';
+
+  @override
+  Future<void> cacheQuestions(
+      QuestionResponseEntity questions, String examId) async {
+    final box = await Hive.openBox(_questionBox);
+    await box.put(examId, questions.toJson());
+  }
+
+  @override
+  Future<QuestionResponseEntity?> getCachedQuestions(String examId) async {
+    final box = await Hive.openBox(_questionBox);
+    final data = box.get(examId);
+    if (data != null) {
+      return QuestionResponseEntity.fromJson(Map<String, dynamic>.from(data));
+    }
+    return null;
+  }
+
+  @override
+  Future<void> cacheCheckQuestions(
+      CheckQuestionResponseEntity checkQuestion) async {
+    final box = await Hive.openBox(_checkQuestionBox);
+    await box.put(_checkQuestionKey, checkQuestion.toJson());
+  }
+
+  @override
+  Future<CheckQuestionResponseEntity?> getCachedCheckQuestions(
+      CheckQuestionRequestEntity checkQuestion) async {
+    final box = await Hive.openBox(_checkQuestionBox);
+    final data = box.get(_checkQuestionKey);
+
+    if (data != null) {
+      return CheckQuestionResponseEntity.fromJson(
+          Map<String, dynamic>.from(data));
+    }
+    return null;
+  }
+}
---
+++ lib/src/features/questions/presentation/widgets/circular_percent.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:online_exam_app/src/core/styles/app_colors.dart';
+import 'package:online_exam_app/src/core/styles/app_styles.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+
+// class CircularPercent extends StatelessWidget {
+//   const CircularPercent({super.key, required this.totalScore});
+//   final String totalScore;
+//   @override
+//   Widget build(BuildContext context) {
+//     return CircularPercentIndicator(
+//       radius: 80.0,
+//       lineWidth: 10.0,
+//       percent: double.parse(totalScore.toString()) / 100,
+//       center: Text(
+//         totalScore.toString() + "%",
+//         style: AppStyles.styleMedium20(context),
+//       ),
+//       progressColor: AppColors.blueBaseColor,
+//       backgroundColor: AppColors.errorColor,
+//       circularStrokeCap: CircularStrokeCap.round,
+//     );
+//   }
+// }
+class CircularPercent extends StatelessWidget {
+  const CircularPercent({super.key, required this.totalScore});
+
+  final String totalScore;
+
+  double _parseTotalScore(String score) {
+    try {
+      return double.parse(score.replaceAll('%', '').trim()) / 100;
+    } catch (e) {
+      return 0.0;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final parsedScore = _parseTotalScore(totalScore);
+
+    return CircularPercentIndicator(
+      radius: 80.0,
+      lineWidth: 10.0,
+      percent: parsedScore,
+      center: Text(
+        "${(parsedScore * 100).toStringAsFixed(2)}%",
+        style: AppStyles.styleMedium20(context),
+      ),
+      progressColor: AppColors.blueBaseColor,
+      backgroundColor: AppColors.errorColor,
+      circularStrokeCap: CircularStrokeCap.round,
+    );
+  }
+}
