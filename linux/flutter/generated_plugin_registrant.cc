168c3852e6577aa941ff6e96a7e58101df34f014
YasmeenGad <gadyasmeen335@gmail.com>
2025-01-20 05:58:11 +0200

    fix: fix some issues😭

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -17,8 +17,8 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) flutter_localization_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterLocalizationPlugin");
   flutter_localization_plugin_register_with_registrar(flutter_localization_registrar);
-    g_autoptr(FlPluginRegistrar)
-    isar_flutter_libs_registrar =
-            fl_plugin_registry_get_registrar_for_plugin(registry, "IsarFlutterLibsPlugin");
-    isar_flutter_libs_plugin_register_with_registrar(isar_flutter_libs_registrar);
+  g_autoptr(FlPluginRegistrar)
+  isar_flutter_libs_registrar =
+          fl_plugin_registry_get_registrar_for_plugin(registry, "IsarFlutterLibsPlugin");
+  isar_flutter_libs_plugin_register_with_registrar(isar_flutter_libs_registrar);
 }
