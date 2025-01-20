195b3a77234b8753f0446d58f6a8ade62548d08d
YasmeenGad <gadyasmeen335@gmail.com>
2025-01-20 05:01:59 +0200

    fix: fix some issues😭

--- lib/src/features/questions/presentation/widgets/circular_percent.dart
+++ lib/src/features/questions/presentation/widgets/circular_percent.dart
@@ -1,27 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:online_exam_app/src/core/styles/app_colors.dart';
 import 'package:online_exam_app/src/core/styles/app_styles.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
 
-// class CircularPercent extends StatelessWidget {
-//   const CircularPercent({super.key, required this.totalScore});
-//   final String totalScore;
-//   @override
-//   Widget build(BuildContext context) {
-//     return CircularPercentIndicator(
-//       radius: 80.0,
-//       lineWidth: 10.0,
-//       percent: double.parse(totalScore.toString()) / 100,
-//       center: Text(
-//         totalScore.toString() + "%",
-//         style: AppStyles.styleMedium20(context),
-//       ),
-//       progressColor: AppColors.blueBaseColor,
-//       backgroundColor: AppColors.errorColor,
-//       circularStrokeCap: CircularStrokeCap.round,
-//     );
-//   }
-// }
 class CircularPercent extends StatelessWidget {
   const CircularPercent({super.key, required this.totalScore});
 
@@ -39,17 +19,6 @@ class CircularPercent extends StatelessWidget {
   Widget build(BuildContext context) {
     final parsedScore = _parseTotalScore(totalScore);
 
-    return CircularPercentIndicator(
-      radius: 80.0,
-      lineWidth: 10.0,
-      percent: parsedScore,
-      center: Text(
-        "${(parsedScore * 100).toStringAsFixed(2)}%",
-        style: AppStyles.styleMedium20(context),
-      ),
-      progressColor: AppColors.blueBaseColor,
-      backgroundColor: AppColors.errorColor,
-      circularStrokeCap: CircularStrokeCap.round,
-    );
+    return Container();
   }
 }
